0c709077c5c510d756f0abd181d7de75004c39df
cororing <corock.dev@gmail.com>
2021-08-20 12:21:56 +0900

    API 문서 추가

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  /** @see https://docs.nestjs.com/openapi/introduction */
+  const config = new DocumentBuilder()
+    .setTitle('PAS Server')
+    .setDescription('Programming Assistant System API description')
+    .setVersion('1.0')
+    .addTag('PAS')
+    .addCookieAuth('connect.sid')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   const port = process.env.PORT || 4000;
   await app.listen(port);
   console.log(`Listening on port ${port}`);
