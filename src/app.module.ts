85cf472c544f351f8d4cdfc39af0de76adc47a6a
cororing <corock.dev@gmail.com>
2021-08-26 14:12:55 +0900

    nest generate resource auth --no-spec

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AppService } from './app.service';
 import { LoggerMiddleware } from './middlewares/logger.middleware';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 import * as ormconfig from '../ormconfig';
 
@@ -13,6 +14,7 @@ import * as ormconfig from '../ormconfig';
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot(ormconfig),
     UsersModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
