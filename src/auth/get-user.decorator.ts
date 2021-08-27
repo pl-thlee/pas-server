6af0dd381ec258577265c4618a10b5e4b88ac31b
cororing <corock.dev@gmail.com>
2021-08-27 10:48:27 +0900

    타입 표시

--- src/auth/get-user.decorator.ts
+++ src/auth/get-user.decorator.ts
@@ -2,7 +2,7 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { User } from 'src/users/entities/user.entity';
 
 export const GetUser = createParamDecorator(
-  (data, ctx: ExecutionContext): User => {
+  (data: unknown, ctx: ExecutionContext): User => {
     const req = ctx.switchToHttp().getRequest();
     return req.user;
   },
