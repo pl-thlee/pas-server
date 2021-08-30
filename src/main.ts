25a8cd46f27f60ee099b67b5c33ece9b70218df0
2paradox <dltjdcjf93@likelion.org>
2021-08-31 05:40:23 +0900

    main.ts에 static 폴더 설정, cors 적용 코드 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import { join } from 'path';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.enableCors();
+  app.useStaticAssets(join(__dirname, '..', 'static'));
   await app.listen(3000);
 }
 bootstrap();
