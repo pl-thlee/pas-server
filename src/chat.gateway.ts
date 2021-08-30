c18340b1c80b595ad5be228038f734d299270bb2
2paradox <dltjdcjf93@likelion.org>
2021-08-31 06:05:35 +0900

    wss initialized 테스트 코드 추가, app.module.ts 코드 최신화

--- src/chat.gateway.ts
+++ src/chat.gateway.ts
@@ -1,7 +1,15 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { Logger } from '@nestjs/common';
+import { OnGatewayInit, SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
 
 @WebSocketGateway()
-export class ChatGateway {
+export class ChatGateway implements OnGatewayInit{
+
+  private logger: Logger = new Logger('ChatGateway');
+
+  afterInit(server: any){
+    this.logger.log("Initialized!");
+  }
+  
   @SubscribeMessage('message')
   handleMessage(client: any, payload: any): string {
     return 'Hello world!';
