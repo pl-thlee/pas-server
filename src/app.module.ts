bd64a1d12b9257944b46ef2f3360436d3e0168af
cororing <corock.dev@gmail.com>
2021-09-16 16:47:32 +0900

    Modify path

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,7 +11,7 @@ import { join } from 'path';
 @Module({
   imports: [
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..'),
+      rootPath: join(__dirname, '../..', 'client'),
     }),
     ConfigModule.forRoot({
       isGlobal: true,
