3cc761b30e9288cb31280ea9e54948f00943b8b8
2paradox <dltjdcjf93@likelion.org>
2021-09-16 14:58:08 +0900

    라이프사이클 관련 함수 추가

--- src/chat.gateway.ts
+++ src/chat.gateway.ts
@@ -1,7 +1,35 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
 
-@WebSocketGateway()
-export class ChatGateway {
+@WebSocketGateway(81, {
+  cors: {
+    origin: 'http://localhost:3000',
+    // methods: ['GET', 'POST'],
+    credential: true,
+  },
+})
+export class ChatGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer()
+  server: Server;
+  async afterInit(server: Server) {
+    console.log('Initialized');
+  }
+  async handleConnection(socket: Socket) {
+    console.log(`Client ${socket.id} connected`);
+  }
+  async handleDisconnect(socket: Socket) {
+    console.log(`Client ${socket.id} disconnected`);
+  }
+
   @SubscribeMessage('message')
   handleMessage(client: any, payload: any): string {
     return 'Hello world!';
