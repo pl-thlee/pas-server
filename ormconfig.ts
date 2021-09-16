bae369d48a437590e557ea98194f7ed03a8453ae
cororing <corock.dev@gmail.com>
2021-09-16 16:34:37 +0900

    Fix typo

--- ormconfig.ts
+++ ormconfig.ts
@@ -10,7 +10,7 @@ const config: TypeOrmModuleOptions = {
   password: process.env.DB_PASS,
   database: process.env.DB_HOST,
   entities: [User],
-  synchronize: true,
+  synchronize: false,
   charset: 'utf8mb4',
   logging: true,
   keepConnectionAlive: true,
